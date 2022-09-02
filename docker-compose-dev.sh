d0d76412dde97bd507cea84df60947ccbc525fd2
mrodac <github@mrod.com.ar>
2022-09-01 22:03:45 -0300

    Ej 2

--- docker-compose-dev.sh
+++ docker-compose-dev.sh
@@ -22,6 +22,8 @@ render_server () {
     container_name: server
     image: server:latest
     entrypoint: python3 /main.py
+    volumes:
+      - ./config/server.ini:/config.ini
     environment:
       - PYTHONUNBUFFERED=1
       - SERVER_PORT=12345
@@ -37,6 +39,8 @@ render_client () {
     container_name: client${1}
     image: client:latest
     entrypoint: /client
+    volumes:
+      - ./config/client.yaml:/config.yaml
     environment:
       - CLI_ID=${1}
       - CLI_SERVER_ADDRESS=server:12345
